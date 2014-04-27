4aac42f356441f4f34844c17214ac4c82e883c45
Teddy <theodore.widom@gmail.com>
2014-04-28 00:12:26 +0800

    vim-rspec is not working...

--- spec/materialized_client_spec.rb
+++ spec/materialized_client_spec.rb
@@ -7,11 +7,20 @@ class UnmaterializedClient < ActiveRecord::Base
 end
 
 describe MaterializedClient do
-  describe 'Stays up to date relative its unmaterialized parent' do
-    specify 'On insertions' do
-      UnmaterializedClient.create(name: 'new client')
-      UnmaterializedClient.count.should == 1
-      MaterializedClient.count.should == 1
+  describe 'Stays up to date' do
+    describe 'Relative to its unmaterialized version' do
+      specify 'On insertions, updates, and deletions' do
+        unm_client = UnmaterializedClient.create(name: 'new client')
+        mat_client = MaterializedClient.find_by(name: 'new client')
+
+        UnmaterializedClient.count.should == 1
+        MaterializedClient.count.should == 1
+
+        MaterializedClient.first.name.should == 'new client'
+        UnmaterializedClient.first.name.should == 'new client'
+
+
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,9 @@ require 'materialized_views'
 dbconfig = YAML::load(File.open('db/config.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
 
-# RSpec.configure do |config|
-#   config.before(:each) do
-#     UnmaterializedClient.delete_all
-#     MaterializedClient.delete_all
-#   end
-# end
+RSpec.configure do |config|
+  config.before(:each) do
+    UnmaterializedClient.delete_all
+    MaterializedClient.delete_all
+  end
+end
